0bca621f0736d6d601d5011f6f796399afba8e0a
Max Prosper <maxim.yaaman@gmail.com>
2017-10-16 01:20:08 +0300

    Populating Queries

--- uzerz/test/test_helper.js
+++ uzerz/test/test_helper.js
@@ -10,9 +10,14 @@ before((done) => {
       console.warn('Warning', error);
     });
 });
- 
+
 beforeEach((done) => {
-  mongoose.connection.collections.users.drop(() => {
-    done();
+  const { users, comments, blogposts } = mongoose.connection.collections;
+  users.drop(() => {
+    comments.drop(() => {
+      blogposts.drop(() => {
+        done();
+      });
+    });
   });
 });
